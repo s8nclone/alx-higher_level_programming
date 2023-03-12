bcd4c9e983c7a0cca7b50ff4bb5e73a2df2586a8
s8nclone <dayojnr.3@gmail.com>
2023-03-12 13:42:45 -0700

    my first toolbox!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    num = len(sys.argv) - 1
+    num = len(argv) - 1
     if num == 0:
-        print("0 arguments.")
-    elif num == 1:
-        print("1 argument:")
+        print("{:d} argument:".format(num))
+        return
     else:
-        print("{} arguments:".format(num))
-        for i in range(num):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        if num == 1:
+            print("{:d} argument:".format(num))
+        else:
+            print("{:d} arguments:".format(num))
+        i = 1
+        while i <= num:
+            print("{:d}: {:s}".format(i, arv[i]))
+            i += 1
+
+    if __name__ == "__main__":
+        import sys
+        print_arg(sys.argv)
