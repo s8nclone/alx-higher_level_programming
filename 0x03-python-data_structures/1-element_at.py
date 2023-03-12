48675c61b146aa44f1dc42691a3b897cc67605cd
s8nclone <dayojnr.3@gmail.com>
2023-03-12 11:14:58 -0700

    replace element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, id):
-    if id < 0:
+def element_at(my_list, idx):
+    if idx < 0:
         return None
-    elif id >= len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        return my_list[id]
+        return my_list[idx]
