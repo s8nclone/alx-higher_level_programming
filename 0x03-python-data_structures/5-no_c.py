3795884e882744c51d7363615f48ce767aa1e376
s8nclone <dayojnr.3@gmail.com>
2023-03-12 12:11:13 -0700

    can you c me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(new_string))
+    new_string = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_string += x
+            return new_string
