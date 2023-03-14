71626740d7f857dfa281076ccaca475a805cb262
s8nclone <dayojnr.3@gmail.com>
2023-03-13 21:27:51 -0700

    inventing is a combination of brain and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -7,6 +7,6 @@
     for num1 in range(0, 9):
         for num2 in range(num1 + 1, 10):
             if num1 == 8 and num2 == 9:
-                print(f'{num1}{num2}')
+                print("{:d}{:d}".format(num1, num2))
             else:
-                print(f'{num1}{num2}', end=", ")
+                print("{:d}{:d}".format(num1, num2), end=", ")
