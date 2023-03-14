16b50848e4c3510e1872790f02656d37fd6abdf4
s8nclone <dayojnr.3@gmail.com>
2023-03-14 08:55:37 -0700

    infinite add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,10 +9,10 @@ def print_arg(argv):
             print("{:d} argument:".format(n))
         else:
             print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+    i = 1
+    while i <= n:
+        print("{:d}: {:s}".format(i, argv[i]))
+        i += 1
 
 if __name__ == "__main__":
     import sys
