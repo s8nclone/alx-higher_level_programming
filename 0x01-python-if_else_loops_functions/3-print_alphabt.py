320d69c15eb04348dfa33952c52b8d94b221bfa6
s8nclone <dayojnr.3@gmail.com>
2023-03-13 21:14:57 -0700

    when i was having alphabet soup, i never thought it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in (97, 123):
+for i in range(97, 123):
     if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print("{:s}".format(chr(i)), end='')
+    print("{:s}".format(chr(i)), end="")
