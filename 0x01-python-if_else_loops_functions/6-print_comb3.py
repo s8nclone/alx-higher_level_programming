4ef45be057144c5d903d9aaadb58dddfc2895a0b
s8nclone <dayojnr.3@gmail.com>
2023-03-14 06:16:49 -0700

    inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Print all possible different combinations of two digits in ascending order.
 
-    The two digits must be different -  01 and 10 are considered the same combinatin of 1 and 0.
+    The two digits must be different -  01 and 10 are considered the same combination.
     """
 
-    for num1 in range(0, 9):
-        for num2 in range(num1 + 1, 10):
-            if num1 == 8 and num2 == 9:
-                print("{:d}{:d}".format(num1, num2))
-            else:
-                print("{:d}{:d}".format(num1, num2), end=", ")
+for num1 in range(0, 9):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{:d}{:d}".format(num1, num2))
+        else:
+            print("{:d}{:d}".format(num1, num2), end=", ")
