84c34fdd3e65fe45ddd9bb2d400e77413b6f8c00
s8nclone <dayojnr.3@gmail.com>
2023-03-13 20:35:00 -0700

    inventing is a combination of brains and materials

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,9 +4,9 @@
     The two digits must be different -  01 and 10 are considered the same combinatin of 1 and 0.
     """
 
-    for num1 in range(0, 10):
-        for num2 in range(num1 = 1, 10):
+    for num1 in range(0, 9):
+        for num2 in range(num1 + 1, 10):
             if num1 == 8 and num2 == 9:
-                print(f{num1}{num2})
+                print(f'{num1}{num2}')
             else:
-                print(f{num1}{num2})
+                print(f'{num1}{num2}', end=", ")
