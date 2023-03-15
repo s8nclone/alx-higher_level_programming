142224aea082decac2458b5e2846fdca197908d6
s8nclone <dayojnr.3@gmail.com>
2023-03-15 10:13:59 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    return ([new_list = replace for i in range(len(new_list)) if search in new_list])
+    new_list = list(map(lambda x: x.replace('search', 'replace'), new_list))
