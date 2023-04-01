393afad58f651e952f597ea017e0da05c612771c
s8nclone <dayojnr.3@gmail.com>
2023-04-01 06:19:26 -0700

    compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines a class square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new square.
+
+        Args:
+            size: The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets and sets the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current area of the square."""
+        return (self.__size * self.__size)
+
+    def __equal__(self, other):
+        """Defines the == comparision to a square."""
+        return self.area() == other.area()
+
+    def __notequal__(self, other):
+        """Defines the != comparison to a square."""
+        return self.area() != other.area()
+
+    def __less__(self, other):
+        """Defines the < comparison to a square."""
+        return self.area() < other.area()
+
+    def __lessequal__(self, other):
+        """Defines the <= comparison to a square."""
+        return self.area() <= other.area()
+
+    def __greater__(self, other):
+        """Defines the > comparison to a square."""
+        return self.area() > other.area()
+
+    def __grtequal__(self, other):
+        """Defines the >= compmarison to a square."""
+        return self.area() >= other.area()
