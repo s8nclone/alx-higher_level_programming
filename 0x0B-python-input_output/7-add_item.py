c398d0ffea635f2eaa8212521c64dec121a9dafa
s8nclone <dayojnr.3@gmail.com>
2023-05-03 21:30:38 -0700

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list and saves them to a file"""
-
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
