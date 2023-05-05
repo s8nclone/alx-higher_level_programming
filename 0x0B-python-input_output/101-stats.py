715b22cfa611d03c8a0612f2cc99ac71d3eb3991
s8nclone <dayojnr.3@gmail.com>
2023-05-05 07:04:27 -0700

    log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,7 +7,7 @@ def print_status():
     counter = 0
     size = 0
     file_size = 0
-     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
     for ln in sys.stdin:
@@ -30,6 +30,7 @@ def print_status():
         for key, val in sorted(status_codes.items()):
             if (val != 0):
                 print("{}: {}".format(key, val))
+
 
 if __name__ == "__main__":
     print_status()
