2d4b1906b7ea507a7b8a2695846278e293240d95
s8nclone <dayojnr.3@gmail.com>
2023-05-05 07:00:47 -0700

    log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,25 +7,29 @@ def print_status():
     counter = 0
     size = 0
     file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-    for lx in sys.stdin:
-        line = lx.split()
-        if len(line) < 2:
-            continue
+    for ln in sys.stdin:
+        line = ln.split()
         try:
             size += int(line[-1])
             code = line[-2]
             status_codes[code] += 1
         except ValueError:
             continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
         counter += 1
-    print("File size: {}".format(size))
-    for key, val in sorted(status_codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
 if __name__ == "__main__":
     print_status()
