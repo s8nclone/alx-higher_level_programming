c42d6b3104ebae8b1d28e15c515d7653fdfcd276
s8nclone <dayojnr.3@gmail.com>
2023-05-05 06:12:13 -0700

    student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class_to_json function."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure
+    for JSON serialization of an object
+
+    obj: instance of a class.
+    """
+    return obj.__dict__
