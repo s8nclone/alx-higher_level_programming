143d1a220b7818898c6f41440f1aa2048855553c
s8nclone <dayojnr.3@gmail.com>
2023-05-05 06:16:09 -0700

    student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""Defines a class_to_json function."""
+"""Defines a Student class"""
 
 
-def class_to_json(obj):
-    """Returns the dictionary description with simple data structure
-    for JSON serialization of an object
+class Student:
+    """Represents the class Student"""
 
-    obj: instance of a class.
-    """
-    return obj.__dict__
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_list = []
+            for i in attrs:
+                if i in self.__dict__:
+                    new_list.append(i)
+            return {x: self.__dict__[x] for x in new_list}
