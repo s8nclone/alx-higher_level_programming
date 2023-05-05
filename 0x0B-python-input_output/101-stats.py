7083af67a4759e801348cba95c25dfc0a9182fd8
s8nclone <dayojnr.3@gmail.com>
2023-05-05 06:47:37 -0700

    log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
 def print_status():
     """Printing the status of the request"""
     counter = 0
@@ -10,27 +9,23 @@ def print_status():
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
-    for l in sys.stdin:
-        line = l.split()
+    for lx in sys.stdin:
+        line = lx.split()
+        if len(line) < 2:
+            continue
         try:
             size += int(line[-1])
             code = line[-2]
             status_codes[code] += 1
-        except:
+        except ValueError:
             continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
         counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
+    print("File size: {}".format(size))
+    for key, val in sorted(status_codes.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
 if __name__ == "__main__":
     print_status()
+
