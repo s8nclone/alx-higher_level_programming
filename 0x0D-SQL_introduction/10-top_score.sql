6089b0b8afe3f1274343f0fafe2a1c05d4ccd37e
s8nclone <dayojnr.3@gmail.com>
2023-05-21 03:28:24 -0700

    list by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Script that lists all records of the table second_table of
 -- the database in MySQL server
 -- Records should be ordered y score (top first)
-SELECT score, FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
