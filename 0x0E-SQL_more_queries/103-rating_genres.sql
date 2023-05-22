cbe67bb982ba43fa7d3281bed9fe101667275537
s8nclone <dayojnr.3@gmail.com>
2023-05-22 12:13:37 -0700

    best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,13 @@
 -- Script that lists all genres in the database hbtn_0d_tvshows_rate by their ratin
 -- Records should display tv_genres.name - rating sum
 -- Results must be sorted in descending order by their rating
-SELECT `name`, SUM(`rate`) AS `rating`
-  	FROM `tv_genres` AS genre
-       	INNER JOIN `tv_show_genres` AS show
-       	ON show.`genre_id` = genre.`id`
-       	INNER JOIN `tv_show_ratings` AS rate
-       	ON rate.`show_id` = show.`show_id`
- 	GROUP BY `name`
- 	ORDER BY `rating` DESC;
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+       FROM tv_genres
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+       INNER JOIN tv_shows
+       ON tv_shows.id = tv_show_genres.show_id
+       INNER JOIN tv_show_ratings
+       ON tv_shows.id = tv_show_ratings.show_id
+       GROUP BY tv_genres.name
+       ORDER BY rating DESC;
