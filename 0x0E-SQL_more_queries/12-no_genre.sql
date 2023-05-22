043f84ad13c9eb012f74e69b3438c90717a05206
s8nclone <dayojnr.3@gmail.com>
2023-05-22 11:28:57 -0700

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Script that lists all sows contained in hbtn_0d_tvsows without a genre linked
+-- Record should display tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order
+SELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_shows LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_show_genres.show_id IS NULL
+	ORDER BY tv_shows.titles, tv_show_genres.genre_id ASC;
