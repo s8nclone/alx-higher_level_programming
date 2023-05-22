c3eebc0750efc1b4894082f44267954ac8e3cbaa
s8nclone <dayojnr.3@gmail.com>
2023-05-22 11:11:37 -0700

    genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 -- Record should display tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order
 SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows INNER JOIN yv_show_enres
+	FROM tv_shows INNER JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 	ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
