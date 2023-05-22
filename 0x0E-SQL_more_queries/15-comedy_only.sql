71c668cf92203cee80cd2a042b3c98393b2d953e
s8nclone <dayojnr.3@gmail.com>
2023-05-22 11:44:52 -0700

    only comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- Script that lists all comedy shows in the database hbtn_0d_tvshows
+--The tv_genres tale contains only one record where name = Comedy
+-- Record must display tv_shows.title
+-- Records must be sorted in ascending order by the show title
+SELECT tv_shows.title AS title
+	FROM tv_show_genres
+	INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+	INNER JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	AND tv_genres.name = 'Comedy'
+	ORDER BY title;
