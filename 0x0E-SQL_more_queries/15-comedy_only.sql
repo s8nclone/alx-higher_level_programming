03c835ac1b973423eda50dc5d393a61c94fd51d0
s8nclone <dayojnr.3@gmail.com>
2023-05-22 11:49:45 -0700

    only comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,11 +2,11 @@
 --The tv_genres tale contains only one record where name = Comedy
 -- Record must display tv_shows.title
 -- Records must be sorted in ascending order by the show title
-SELECT tv_shows.title As titles
+SELECT tv_shows.title
 	FROM tv_shows
 	INNER JOIN tv_show_genres
 	ON tv_show_genres.show_id = tv_shows.id
 	INNER JOIN tv_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 	AND tv_genres.name = 'Comedy'
-	ORDER BY titles;
+	ORDER BY tv_shows.title;
