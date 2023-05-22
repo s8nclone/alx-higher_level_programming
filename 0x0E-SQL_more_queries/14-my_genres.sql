24b1dd287683ba1449f9ec4e8abc8f0e057684a7
s8nclone <dayojnr.3@gmail.com>
2023-05-22 11:40:37 -0700

    my genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,13 @@
+-- Script that uses the hbtn_0d_tvshows databse to lists all
+-- genres of the show Dexter.
+-- The tv_shows table contains only one record where title = Dexter
+-- Record should display tv_genres.name
+-- Results must be sorted in ascending order by genre name
+SELECT tv_genres.name AS name
+	FROM tv_show_genres
+	INNER JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	INNER JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	AND tv_shows.title = 'Dexter'
+	ORDER BY name;
