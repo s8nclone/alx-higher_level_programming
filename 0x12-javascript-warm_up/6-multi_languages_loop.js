cae7d082466fdbfc39bb074e311a74bda98a231a
@s8nclone <dayojnr.3@gmail.com>
2023-06-16 23:46:53 -0700

    loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arrayOfVals = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i <= arrayOfVals.length; i++) {
+for (let i = 0; i < arrayOfVals.length; i++) {
   console.log(arrayOfVals[i]);
 }
