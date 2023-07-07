a6af72a76fb356423f37d6f070656159a1f90dcd
s8nclone <dayojnr.3@gmail.com>
2023-07-07 08:47:01 -0700

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  Lists all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     crsr = db.cursor()
-    crsr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    match = sys.argv[4]
+    crsr.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = crsr.fetchall()
     for row in rows:
         print(row)
