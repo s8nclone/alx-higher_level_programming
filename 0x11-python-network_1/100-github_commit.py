f286b66fb2a171e00f9db5ba3f11e7040a7b3287
s8nclone <dayojnr.3@gmail.com>
2023-07-09 21:14:05 -0700

    time for an interview

---
+++ 0x11-python-network_1/100-github_commit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to solve given challenge."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    msg = requests.get(url)
+    commits = msg.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
