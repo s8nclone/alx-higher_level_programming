0049466fb498fe05b0bc7ab93b7ceb51e7a6f467
s8nclone <dayojnr.3@gmail.com>
2023-08-16 20:27:55 -0700

    star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const epNum = process.argv[2];
 const apiUrl = 'https://swapi-api.alx-tools.com/api/films/:id';
 
 request(apiUrl + epNum, function (err, response, body) {
-  if (error == null) {
+  if (err == null) {
     const parsed = JSON.parse(body);
     console.log(parsed.title);
   }
