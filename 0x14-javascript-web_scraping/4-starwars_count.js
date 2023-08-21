88cce32b345db9abcd02e1075390f29a7663d8d5
s8nclone <dayojnr.3@gmail.com>
2023-08-20 18:25:09 -0700

    star wars wedge antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const argv = process.argv[2];
-const api = 'https://swapi-api.alx-tools.com/api/films';
 const xterId = '18';
 let count = 0;
 
-request.get(api + argv, (err, response, body) => {
+request.get(argv, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
