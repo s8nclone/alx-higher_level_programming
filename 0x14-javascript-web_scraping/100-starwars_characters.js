f11228a9a28103ca65b0f495fdbf8aef5a9562e1
s8nclone <dayojnr.3@gmail.com>
2023-08-20 18:05:34 -0700

    who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const argv = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
 request.get(url + argv, function (err, res, body) {
   if (err) {
     console.log(err);
   }
   const data = JSON.parse(body);
-  const dc = data.characters;
-  for (const i of dc) {
-    request.get(i, function (err, res, body1) {
+  const xters = data.characters;
+  for (const i of xters) {
+    request.get(i, function (err, res, xbody) {
       if (err) {
         console.log(err);
       }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+      const dataX = JSON.parse(xbody);
+      console.log(dataX.name);
     });
   }
 });
