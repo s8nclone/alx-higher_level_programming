4a1b69d9ff84e1516364b25cfa27c2c3145f381d
s8nclone <dayojnr.3@gmail.com>
2023-08-20 17:56:25 -0700

    riht order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,11 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const api = 'https://swapi-api.alx-tools.com/api/films/';
+const api = `https://swapi-api.alx-tools.com/api/films/`;
 const argv = process.argv[2];
 
 request(api + argv, function (error, response, body) {
-  if (error === null) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    setOrder(characters, 0);
+    setCharacters(characters, 0);
   }
 });
+
+function setCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        setCharacters(characters, index + 1);
+      }
+    }
+  });
+}
