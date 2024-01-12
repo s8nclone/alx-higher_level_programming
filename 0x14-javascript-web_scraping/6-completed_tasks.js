e6148bc6b11bc3c6c5d361089b88ee0c26a72bee
s8nclone <dayojnr.3@gmail.com>
2024-01-11 19:43:04 -0800

    how many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const argv = process.argv[2];
+const result = {};
+request.get(argv, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    body = JSON.parse(body);
+    for (let i = 0; i < body.length; i++) {
+      const todo = body[i];
+      if (todo.completed === true) {
+        if (!result[todo.userId]) {
+          result[todo.userId] = 1;
+        } else {
+          result[todo.userId] += 1;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
